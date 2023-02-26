b1f66c1aa934c6a06c916c4545a889b8496984d9
Arjun Kochhar <kochhararjun11@gmail.com>
2023-02-26 13:30:34 -0700

    Incorrectly overwrote my file in previous commit

--- f_prime_ghost_cells.cpp
+++ f_prime_ghost_cells.cpp
@@ -1,75 +1,120 @@
-//-----------------------------------------------------
+/-----------------------------------------------------
 // Arjun Kochhar, Feb 2023
 //
-// Program to initialize non-ghost cells in a 2d grid
+// Program to initialize non-ghost cells in a 1d-grid
+// and calculate the derivative of a function.
 //-----------------------------------------------------
 
 #include <iostream>
 #include <vector>
 #include <iomanip>
 
-const int N = 2;
-std::vector<float> vec((N + 2) * (N + 2));
 
-int index(float x, float y)
+// function
+float f_x(float x)
 {
-    return y * (N+2) + x;
+    return x * x;
 }
 
-void initalize_non_ghost_cells(std::vector<float>& vec)
+// create vectors
+const int N = 30;
+
+// initalize vectors
+void initalize_x(float low, float high, int N,
+                                std::vector<float>& x_vec)
 {
-    for (int x = 1; x <= N; ++x)
+    auto dl = (high - low) / N;
+    for (int i = 1; i <= N; i++)
     {
-        for (int y = 1; y <= N; ++y)
-            vec[index(x, y)] = 1;
+        auto j = low + i * dl;
+        x_vec[i] = j -  dl / 2;
     }
 }
+
+// initalize vectors
+void initalize_y(const std::vector<float>& x_vec,
+                                std::vector<float>& y_vec)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        y_vec[i] = f_x(x_vec[i]);
+    }
+}
+
+// initalize ghost cells
 
 void initalize_ghost_cells(std::vector<float>& vec)
 {
-    for (int x = 1; x <= N; ++x)
-    {
-        vec[index(x, 0)] = vec[index(x, 1)];
-        vec[index(x, N + 1)] = vec[index(x, N)];
-    }
-
-    for (int y = 1; y <= N; ++y)
-    {
-        vec[index(0, y)] = vec[index(1, y)];
-        vec[index(N + 1, y)] = vec[index(N, y)];
-    }
-
-    // initalize corners
-    vec[index(0, 0)] = (vec[index(1, 0)] + vec[index(0, 1)]) / 2;
-    vec[index(0, N +  1)] = (vec[index(1, N + 1)] + vec[index(0, N)]) / 2;
-    vec[index(N + 1, N + 1)] = (vec[index(N + 1, N)] + vec[index(N, N + 1)]) / 2;
-    vec[index(N + 1, 0)] = (vec[index(N, 0)] + vec[index(N + 1, 1)]) / 2;
+    vec[0] = vec[1];
+    auto n = vec.size() - 2;
+    vec[n + 1] = vec[n];
 }
 
-void print_grid(const std::vector<float>& vec)
+void numerically_calculate_derivative(const std::vector<float>& x,
+                                        const std::vector<float>& y,
+                                        std::vector<float>& z)
 {
-    std::cout << std::fixed << std::setprecision(3); 
-    std::cout << "vec size = " << vec.size() << std::endl;
-    for (int count = 0; auto i: vec)
+    for (int i = 1; i <= N; i++)
     {
-        std::cout << i << ", ";
-        ++count;
-        if (count % (N + 2) == 0)
-            std::cout << std::endl;
+        auto dx = x[i + 1] - x[i - 1];
+        auto dy = y[i + 1] - y[i - 1];
+        auto derivative = dy / dx;
+        z[i] = derivative;
     }
+}                                    
+
+void print_vector(const std::vector<float> &vec)
+{
+    // max of ten values in one line
+    std::cout << std::fixed << std::setprecision(3);
+    int count = 1;
+    for (auto i: vec)
+    {
+        if ((count % 10) == 0)
+        {
+            std::cout << std::endl;
+        }
+        std::cout << i << ", ";
+        count++;
+    }
+    std::cout << std::endl;
+}
+
+void print_vectors(const std::vector<float> &x_vec,
+    const std::vector<float> &y_vec,
+    const std::vector<float> &z_vec)
+{
+    // print vectors
+    std::cout << "X VECTOR: ";
+    print_vector(x_vec);
+
+    std::cout << "Y VECTOR: ";
+    print_vector(y_vec);
+
+    std::cout << "Z VECTOR: ";
+    print_vector(z_vec);
 }
 
 int main()
 {
-    initalize_non_ghost_cells(vec);
-    std::cout << "Grid with out ghost cell initialization" << std::endl;
-    print_grid(vec);
+    // initalize vectors:
+    std::vector<float> x_vec(N + 2);
+    std::vector<float> y_vec(N + 2);
+    std::vector<float> z_vec(x_vec.size());    
 
-    std::cout << "------------------------" << std::endl;
-    initalize_ghost_cells(vec);
+    initalize_x(-2, 2, N, x_vec);
+    initalize_y(x_vec, y_vec);
+    std::cout << "Cells before ghost cell initialization" << std::endl;
+    print_vectors(x_vec, y_vec, z_vec);
+    std::cout << std::endl << std::endl;
 
-    std::cout << "Grid with ghost cell initialization" << std::endl;
-    print_grid(vec);
+    initalize_ghost_cells(x_vec);
+    initalize_ghost_cells(y_vec);
+    numerically_calculate_derivative(x_vec, y_vec, z_vec);
+
+    std::cout << "Cells after ghost cell initialization" << std::endl;
+    print_vectors(x_vec, y_vec, z_vec);
 
     return 0;
 }
+
